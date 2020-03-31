74ffa60fea981f2ce9e375cec4cd54b2642336fc
AnhMH <anh.mh@moziasoft.com>
2020-03-31 22:21:30 +0700

    Add sale off page

--- src/Controller/SaleoffsController.php
+++ src/Controller/SaleoffsController.php
@@ -14,24 +14,27 @@ class SaleoffsController extends AppController {
      * List products
      */
     public function index() {
-        $limit = 18;
+        $limit = 16;
         $param = $this->getParams(array(
             'limit' => $limit,
             'disable' => 0,
-            'is_sale_off' => 1,
-            'sort' => 'discount_price-desc'
+            'is_discount' => 1,
+            'sort' => 'discount_price-desc',
+            'get_new_products' => 1
         ));
         $pageTitle = $cateName = 'Khuyến mãi';
         $result = Api::call(Configure::read('API.url_products_list'), $param);
         $total = !empty($result['total']) ? $result['total'] : 0;
         $data = !empty($result['data']) ? $result['data'] : array();
+        $newProducts = !empty($result['new_products']) ? $result['new_products'] : array();
         $this->set(compact(
             'data',
             'limit',
             'total',
             'param',
             'cateName',
-            'pageTitle'
+            'pageTitle',
+            'newProducts'
         ));
     }
 }
